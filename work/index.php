8cd9bda488291a128b69f5b95a013586359df4e6
nakamura <gkii.8r@gmail.com>
2021-05-29 14:11:39 +0900

    #6

--- work/index.php
+++ work/index.php
@@ -2,14 +2,12 @@
 $n = mt_rand(1, 3);
 
 if ($n === 1) {
-  $message = '<script>alert(1);</script>UI/UXデザイナー見習いです';
+  $message = 'UI/UXデザイナー見習いです';
 } elseif ($n === 2) {
   $message = 'プログラミング勉強中です';
 } else {
   $message = 'PHPちょっとわかります';
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +29,9 @@ if ($n === 1) {
       </div>
       <div class="info">
         <h1>山田太郎</h1>
-        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p>
+          <?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?>
+        </p>
         <ul>
           <li>
             <a href="https://dotinstall.com" target="_blank">
